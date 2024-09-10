8d10a2d8f4fbface49a577a96bed67e7d8abd58f
ozeanbrandung <natalia.v.molchanova@yandex.ru>
2024-09-10 23:51:41 +0400

    add auth init; user init to api

--- api-service/src/main.ts
+++ api-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  //domain:port/api
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+    //enable cookies etc
+    credentials: true,
+    //allowed headers
+    exposedHeaders: 'set-cookie',
+  });
+  await app.listen(process.env.API_PORT);
 }
 bootstrap();
